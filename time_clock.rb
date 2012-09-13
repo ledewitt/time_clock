36c90a30492f4ca684f020240f75070e1da7ba23
Luke DeWitt <ledewitt@gmail.com>
2012-09-13 11:07:55 -0600

    Adding pages and some functionality.

--- time_clock.rb
+++ time_clock.rb
@@ -5,5 +5,20 @@ require "sinatra/reloader" if development?
 get('/') {
   time_now = Time.now
   
-  erb :home, locals: { time_now: time_now }
+  if params[:name]
+    #check to see if it exists in database as well
+  else
+    redirect '/login'
+  end
+    
+  erb :home, locals: { time_now: time_now,
+                       name:     (params[:name]) }
+}
+
+get('/join') {
+  erb :join
+}
+
+get('/login') {
+  erb :login
 }
